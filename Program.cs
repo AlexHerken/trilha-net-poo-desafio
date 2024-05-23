bd4266aab6813a25f004169f363e7c1a2745dfdb
Alex <97568537+AlexHerken@users.noreply.github.com>
2024-05-23 16:12:04 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,25 @@
-﻿using DesafioPOO.Models;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+namespace DesafioPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nokia nokia = new Nokia("123456789", "Nokia 3310", "123456789", "16GB");
+            Iphone iphone = new Iphone("987654321", "Iphone 12", "987654321", "128GB");
+
+            nokia.Ligar();
+            nokia.ReceberLigacao();
+            nokia.InstalarAplicativo("WhatsApp");
+
+            iphone.Ligar();
+            iphone.ReceberLigacao();
+            iphone.InstalarAplicativo("Instagram");
+        }
+    }
+}
+
+
+
